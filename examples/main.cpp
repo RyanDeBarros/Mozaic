7c7c818f29561b6b3bd2ce98e977336e63f0c3b4
RyanDeBarros <ryan2004db@gmail.com>
2024-10-05 16:48:32 -0400

    Beginning implementation of buffer2d and some more implementation of array.

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,5 +1,7 @@
 #include "include/registry.hpp"
 #include "include/copy_ptr.hpp"
+#include "include/array.hpp"
+#include "include/buffers.hpp"
 
 #include <iostream>
 
@@ -50,29 +52,91 @@ struct MyChild : public MyStruct
 	virtual void print() const override { std::cout << "x=" << x << ", y=" << (y ? "true" : "false") << ", s=\"" << s << "\"" << std::endl; }
 };
 
+static void print_buf(const mozaic::buffer2d& buf)
+{
+	for (mozaic::ssize_t row = buf.height() - 1; row >= 0; --row)
+	{
+		for (size_t col = 0; col < buf.stride(); ++col)
+		{
+			std::cout << static_cast<char>(buf.buffer()[row * buf.stride() + col]) << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
 int main()
 {
 	std::cout << "Welcome to Mozaic!" << std::endl;
 	
-	mozaic::registry<MyStruct, unsigned short, Constructor1, Constructor2> reg;
+	//mozaic::registry<MyStruct, unsigned short, Constructor1, Constructor2> reg;
 	
-	auto h1 = reg.construct(Constructor1{ 3.0f });
-	auto h2 = reg.construct(Constructor2{ true });
+	//auto h1 = reg.construct(Constructor1{ 3.0f });
+	//auto h2 = reg.construct(Constructor2{ true });
 
-	reg.get(h1)->print();
-	reg.get(h2)->print();
+	//reg.get(h1)->print();
+	//reg.get(h2)->print();
 
-	mozaic::copy_ptr<MyChild> p1(new MyChild(1.0f, true, "hi"));
-	p1->print();
-	mozaic::copy_ptr<MyStruct> p2;
-	p2 = p1;
-	p1->x = 3.0f;
-	p1->print();
-	p2->print();
+	//mozaic::copy_ptr<MyChild> p1(new MyChild(1.0f, true, "hi"));
+	//p1->print();
+	//mozaic::copy_ptr<MyStruct> p2;
+	//p2 = p1;
+	//p1->x = 3.0f;
+	//p1->print();
+	//p2->print();
 
-	const mozaic::copy_ptr<MyStruct> p3(4.0f, false);
-	p3->print();
+	//const mozaic::copy_ptr<MyStruct> p3(4.0f, false);
+	//p3->print();
+
+	size_t w = 4;
+	size_t h = 5;
+	char* buf_data = new char[w * h * 1]{
+		'a', 'b', 'c', 'd',
+		'e', 'f', 'g', 'h',
+		'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p',
+		'q', 'r', 's', 't'
+	};
+	mozaic::buffer2d buf((std::byte*)buf_data, w, h, 1);
+	print_buf(buf);
+	std::cout << std::endl;
+	buf.flip_vertically();
+	print_buf(buf);
+	std::cout << std::endl;
+	buf.flip_horizontally();
+	print_buf(buf);
+	std::cout << std::endl;
+	buf.flip_vertically();
+	print_buf(buf);
+	std::cout << std::endl;
 	
+	mozaic::horizontal_line hline;
+	hline.y = 1;
+	hline.x0 = 1;
+	hline.x1 = 3;
+	char p0[1] = {'x'};
+	buf.set((std::byte*)p0, hline);
+	print_buf(buf);
+	std::cout << std::endl;
+
+	mozaic::vertical_line vline;
+	vline.x = 0;
+	vline.y0 = 1;
+	vline.y1 = 4;
+	char p1[1] = { 'y' };
+	buf.set((std::byte*)p1, vline);
+	print_buf(buf);
+	std::cout << std::endl;
+
+	mozaic::upright_rect urect;
+	urect.x0 = 1;
+	urect.x1 = 2;
+	urect.y0 = 0;
+	urect.y1 = 3;
+	char p2[1] = { 'z' };
+	buf.set((std::byte*)p2, urect);
+	print_buf(buf);
+	std::cout << std::endl;
+
 	std::cin.get();
 	return 0;
 }
--- include/array.hpp
+++ include/array.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <stdexcept>
+#include <type_traits>
 #include <string>
+#include <utility>
 
+// TODO add optional index access range checking. could be a wrapper arround array/var_array
 namespace mozaic
 {
+	typedef std::make_signed_t<size_t> ssize_t;
+
 	// TODO polymorphic subtypes
 	template<typename T, size_t Len, bool Initialize = true>
 	class array
 	{
-		template<typename U>
+		template<typename U, size_t L, bool I>
 		friend class array;
 
 		T* _arr = nullptr;
@@ -33,12 +38,12 @@ namespace mozaic
 		void copy(size_t pos, T* arr, size_t len);
 		void move(size_t pos, T* arr, size_t len);
 		template<size_t SubLen, bool SubInit = Initialize> array<T, SubLen, SubInit> subarray(size_t pos) const;
-		template<bool I> void swap(array<T, Len, I>& other);
+		template<bool I> void swap(array<T, Len, I>& other) noexcept;
 
-		struct bad_length_error : std::runtime_exception
+		struct bad_length_error : std::runtime_error
 		{
-			bad_length_error(const std::string& message = "Array length is incompatible with allocated length (" + std::to_string(Len) + ")") : std::runtime_exception(message) {}
-			bad_length_error(size_t len) : std::runtime_exception("Array length (" + std::to_string(len) + ") is incompatible with allocated length (" + std::to_string(Len) + ")") {}
+			bad_length_error(const std::string& message = "array length is incompatible with allocated length (" + std::to_string(Len) + ")") : std::runtime_error(message) {}
+			bad_length_error(size_t len) : std::runtime_error("array length (" + std::to_string(len) + ") is incompatible with allocated length (" + std::to_string(Len) + ")") {}
 		};
 	};
 	template<typename T, size_t Len, bool Initialize>
@@ -62,10 +67,9 @@ namespace mozaic
 		_arr = raw_heap_array;
 	}
 	template<typename T, size_t Len, bool Initialize>
-	inline array<T, Len, Initialize>::array(const T& val)
+	inline array<T, Len, Initialize>::array(const T& val) : _arr(new T[Len](val))
 	{
 		static_assert(Initialize, "Cannot initialize non-initializing array.");
-		_arr = new T[Len](val);
 	}
 	template<typename T, size_t Len, bool Initialize>
 	template<typename ...Args>
@@ -135,17 +139,246 @@ namespace mozaic
 	}
 	template<typename T, size_t Len, bool Initialize>
 	template<bool I>
-	inline void array<T, Len, Initialize>::swap(array<T, Len, I>& other)
+	inline void array<T, Len, Initialize>::swap(array<T, Len, I>& other) noexcept
 	{
-		T* temp = _arr;
-		_arr = other._arr;
-		other._arr = temp;
+		std::swap(_arr, other._arr);
 	}
 
 	template<typename T>
 	class var_array
 	{
-		// TODO
+		// TODO polymorphic subtypes
 		// subarray can return a variable run-time lengthed array
+		template<typename U>
+		friend class var_array;
+
+		T* _arr = nullptr;
+		size_t _len;
+
+	public:
+		var_array(size_t len = 0, bool initialize = true);
+		~var_array();
+		explicit var_array(T* raw_heap_array, size_t len);
+		explicit var_array(const T& val, size_t len);
+		var_array(const var_array<T>& other);
+		var_array(var_array<T>&& other) noexcept;
+		var_array& operator=(const var_array<T>& other);
+		var_array& operator=(var_array<T>&& other) noexcept;
+		operator bool() const { return static_cast<bool>(_len); }
+		T* get() { return _arr; }
+		const T* get() const { return _arr; }
+		size_t length() const { return _len; }
+		T& operator[](size_t i) { return _arr[i]; }
+		const T& operator[](size_t i) const { return _arr[i]; }
+		template<bool GrowToFit = false> void copy(size_t pos, T* arr, size_t len);
+		template<bool GrowToFit = false> void move(size_t pos, T* arr, size_t len);
+		void resize(size_t len, bool initialize = true);
+		var_array<T> subarray(size_t pos, size_t len) const;
+		var_array<T> subwindow(ssize_t pos, size_t len, bool initialize = true) const;
+		void swap(var_array<T>& other) noexcept;
 	};
+	template<typename T>
+	inline var_array<T>::var_array(size_t len, bool initialize) : _arr(initialize ? new T[len]() : new T[len]), _len(len)
+	{
+	}
+	template<typename T>
+	inline var_array<T>::~var_array()
+	{
+		delete[] _arr;
+	}
+	template<typename T>
+	inline var_array<T>::var_array(T* raw_heap_array, size_t len) : _arr(raw_heap_array), _len(len)
+	{
+	}
+	template<typename T>
+	inline var_array<T>::var_array(const T& val, size_t len) : _arr(new T[len](val)), _len(len)
+	{
+	}
+	template<typename T>
+	inline var_array<T>::var_array(const var_array<T>& other) : _arr(new T[other._len]), _len(other._len)
+	{
+		for (size_t i = 0; i < _len; ++i)
+			new (_arr + i) T(other._arr[i]);
+	}
+	template<typename T>
+	inline var_array<T>::var_array(var_array<T>&& other) noexcept : _arr(other._arr), _len(other._len)
+	{
+		other._arr = nullptr;
+		other._len = 0;
+	}
+	template<typename T>
+	inline var_array<T>& var_array<T>::operator=(const var_array<T>& other)
+	{
+		if (_arr != other._arr)
+		{
+			if (_len == other._len)
+			{
+				for (size_t i = 0; i < _len; ++i)
+				{
+					_arr[i].~T();
+					new (_arr + i) T(other._arr[i]);
+				}
+			}
+			else
+			{
+				delete[] _arr;
+				_len = other._len;
+				_arr = new T[_len];
+				for (size_t i = 0; i < _len; ++i)
+					new (_arr + i) T(other._arr[i]);
+			}
+		}
+		return *this;
+	}
+	template<typename T>
+	inline var_array<T>& var_array<T>::operator=(var_array<T>&& other) noexcept
+	{
+		if (_arr != other._arr)
+		{
+			delete[] _arr;
+			_arr = other._arr;
+			_len = other._len;
+			other._arr = nullptr;
+			other._len = 0;
+		}
+		return *this;
+	}
+	template<typename T>
+	template<bool GrowToFit>
+	inline void var_array<T>::copy(size_t pos, T* arr, size_t len)
+	{
+		len += pos;
+		if constexpr (GrowToFit)
+		{
+			if (len <= _len)
+			{
+				for (size_t i = pos; i < len; ++i)
+					_arr[i] = arr[i];
+			}
+			else
+			{
+				T* temp = new T[len];
+				for (size_t i = 0; i < pos; ++i)
+					temp[i] = std::move(_arr[i]);
+				for (size_t i = pos; i < len; ++i)
+					temp[i] = arr[i];
+				delete[] _arr;
+				_arr = temp;
+				_len = len;
+			}
+		}
+		else
+		{
+			for (size_t i = pos; i < _len && len; ++i)
+				_arr[i] = arr[i];
+		}
+	}
+	template<typename T>
+	template<bool GrowToFit>
+	inline void var_array<T>::move(size_t pos, T* arr, size_t len)
+	{
+		len += pos;
+		if constexpr (GrowToFit)
+		{
+			if (len <= _len)
+			{
+				for (size_t i = pos; i < len; ++i)
+					_arr[i] = arr[i];
+			}
+			else
+			{
+				T* temp = new T[len];
+				for (size_t i = 0; i < pos; ++i)
+					temp[i] = std::move(_arr[i]);
+				for (size_t i = pos; i < len; ++i)
+					temp[i] = std::move(arr[i]);
+				delete[] _arr;
+				_arr = temp;
+				_len = len;
+			}
+		}
+		else
+		{
+			for (size_t i = pos; i < _len && len; ++i)
+				_arr[i] = std::move(arr[i]);
+		}
+	}
+	template<typename T>
+	inline void var_array<T>::resize(size_t len, bool initialize)
+	{
+		if (len < _len)
+		{
+			T* temp = new T[len];
+			for (size_t i = 0; i < len; ++i)
+				temp[i] = std::move(_arr[i]);
+			delete[] _arr;
+			_arr = temp;
+			_len = len;
+		}
+		else if (len > _len)
+		{
+			T* temp = new T[len];
+			for (size_t i = 0; i < _len; ++i)
+				temp[i] = std::move(_arr[i]);
+			if (initialize)
+			{
+				for (size_t i = _len; i < len; ++i)
+					new (temp + i) T();
+			}
+			delete[] _arr;
+			_arr = temp;
+			_len = len;
+		}
+	}
+	template<typename T>
+	inline var_array<T> var_array<T>::subarray(size_t pos, size_t len) const
+	{
+		if (pos > _len)
+			return var_array<T>(nullptr, 0);
+		if (pos + len > _len)
+			len = _len - pos;
+		T* arr = new T[len];
+		for (size_t i = 0; i < len; ++i)
+			arr[i] = _arr[pos + i];
+		return var_array<T>(arr, len);
+	}
+	template<typename T>
+	inline var_array<T> var_array<T>::subwindow(ssize_t pos, size_t len, bool initialize) const
+	{
+		T* temp = new T[len];
+		if (initialize)
+		{
+			for (ssize_t i = 0; i < len && i + pos < 0; ++i)
+				new (temp + i) T();
+		}
+		for (size_t i = 0; i < _len && i < len + pos; ++i)
+			new (temp + i - pos) T(_arr[i]);
+		if (initialize)
+		{
+			for (ssize_t i = 0; i < len + pos - _len; ++i)
+				new (temp + i + _len - pos) T();
+		}
+		return var_array<T>(temp, len);
+	}
+	template<typename T>
+	inline void var_array<T>::swap(var_array<T>& other) noexcept
+	{
+		std::swap(_arr, other._arr);
+		std::swap(_len, other._len);
+	}
+}
+
+namespace std
+{
+	template<typename T>
+	inline void swap(mozaic::var_array<T>& a, mozaic::var_array<T>& b) noexcept
+	{
+		a.swap(b);
+	}
+
+	template<typename T, size_t Len, bool I1, bool I2>
+	inline void swap(mozaic::array<T, Len, I1>& a, mozaic::array<T, Len, I2>& b) noexcept
+	{
+		a.swap(b);
+	}
 }
